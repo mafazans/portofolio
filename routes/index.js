c584ec1f6cead929d85546df3f1552ecff9b44d5
mafazans <arif.mafazan@gmail.com>
2018-02-21 11:20:02 +0700

    add register+ login form and register validation + flash messages

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController')
+const homeController = require('../controllers/homeController');
+const userController = require('../controllers/userController');
+
 
 router.get('/', homeController.homePage);
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
