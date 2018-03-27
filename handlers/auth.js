081bd00f2112b4cd3e584a5370d7fb95c09662ef
mafazans <arif.mafazan@gmail.com>
2018-03-27 09:29:17 +0700

    auth.js

---
+++ handlers/auth.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1757676437609193', // your App ID
+        'clientSecret'  : 'd90645ad9dd0d5bcfd450df62c4a6c91', // your App Secret
+        'callbackURL'   : 'https://mafazans.herokuapp.com/auth/facebook/callback',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
